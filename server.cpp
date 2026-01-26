3c87a7451b5223aaabd9be71ad226c8bd71777a0
PrithviRajDinesh <prithviraj.dinesh@gmail.com>
2026-01-26 14:23:18 +0530

    Added Multiple CLient Functionality

--- server.cpp
+++ server.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <array>
 #include <vector>
+#include <map>
 #include <cstring> 
 #include <boost/asio.hpp>
 #include <boost/asio/posix/stream_descriptor.hpp>
@@ -59,6 +60,19 @@ private:
                             NULL, ciphertext, ciphertext_len, NULL, 0,
                             nonce, key_.data()) == 0) {
 
+                        // Extract the client's VPN IP from the packet
+                        if(decrypted_len >= 20){
+                            std::string client_ip = extract_source_ip(decrypted_buffer_.data());
+
+                            //Save the client in our map
+                            clients_[client_ip] = remote_endpoint_;
+
+                            //Log the connection
+                            std::cout << "Client " << client_ip << " from " 
+                                      << remote_endpoint_.address() << ":" 
+                                      << remote_endpoint_.port() << std::endl;
+                        }
+
                         // Write the decrypted IP Packet to TUN device
                         boost::asio::async_write(tun_fd_, 
                             boost::asio::buffer(decrypted_buffer_.data(), decrypted_len),
@@ -76,6 +90,19 @@ private:
             [this](const boost::system::error_code& ec, std::size_t length) {
                 if (!ec && length > 0) {
                     
+                    //Extract destination IP from packets
+                    std::string dest_ip = extract_dest_ip((unsigned char*)tun_buffer_.data());
+
+                    //Find the client endpoint for this destination
+                    auto it = clients_.find(dest_ip);
+                    if(it == clients_.end()){
+                        //Client was not found, skip this packet
+                        start_tun_read();
+                        return;
+                    }
+
+                    udp::endpoint target_endpoint = it->second;
+
                     // Generate a random unique nonce
                     unsigned char nonce[crypto_aead_chacha20poly1305_ietf_NPUBBYTES];
                     randombytes_buf(nonce, sizeof(nonce));
@@ -94,11 +121,27 @@ private:
 
                     // Send to the last known remote endpoint (client)
                     socket_.async_send_to(
-                        boost::asio::buffer(encrypted_packet, total_send_len), remote_endpoint_,
+                        boost::asio::buffer(encrypted_packet, total_send_len), target_endpoint,
                         [](const boost::system::error_code& /*ec*/, std::size_t /*bytes*/) {});
                 }
                 start_tun_read(); // Restart the loop
             });
+    }
+
+    //Extract source IP from the IPV4 packet (bytes 12-15);
+    std::string extract_source_ip(const unsigned char* packet){
+      char ip_str[16];
+      snprintf(ip_str, sizeof(ip_str), "%d.%d.%d.%d",
+          packet[12], packet[13], packet[14], packet[15]);
+      return std::string(ip_str);
+    }
+
+    //Extract destination IP from the IPV4 packet (bytes 16-19)
+    std::string extract_dest_ip(const unsigned char* packet){
+      char ip_str[16];
+      snprintf(ip_str, sizeof(ip_str), "%d.%d.%d.%d",
+          packet[16], packet[17], packet[18], packet[19]);
+      return std::string(ip_str);
     }
 
     int open_tun(const std::string& dev) {
@@ -125,6 +168,8 @@ private:
     boost::asio::posix::stream_descriptor tun_fd_;
     std::array<unsigned char, crypto_aead_chacha20poly1305_ietf_KEYBYTES> key_;
 
+    std::map<std::string, udp::endpoint> clients_;
+
     std::array<char, 2048> net_buffer_;
     std::array<char, 2048> tun_buffer_;
     std::array<unsigned char, 2048> decrypted_buffer_;
@@ -139,9 +184,10 @@ int main() {
         SecureVpnServer server(io_context, port, device_name);
 
         std::cout << "==========================================" << std::endl;
-        std::cout << "  C++ Secure VPN Server is now running" << std::endl;
+        std::cout << "  Multi-Client VPN Server Running" << std::endl;
         std::cout << "  Listening on UDP Port: " << port << std::endl;
         std::cout << "  Virtual Interface:     " << device_name << std::endl;
+        std::cout << "  Supports Multiple Clients" << std::endl;
         std::cout << "  (Press Ctrl+C to stop)" << std::endl;
         std::cout << "==========================================" << std::endl;
 
